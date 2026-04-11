0acaf75b730386a16db5e7a36119e3004adf2578
tohurtv <joshwebb84@outlook.com>
2026-04-11 08:48:31 -0600

    remove rakuos-autostart from live iso

--- config/configure-iso.sh
+++ config/configure-iso.sh
@@ -1,2 +1,5 @@
 # Install packages for installation
 dnf5 install -y libblockdev-btrfs
+
+#remove rakuos-welcome automatic launch for live environment
+rm -f /etc/xdg/autostart/rakuos-welcome.desktop
