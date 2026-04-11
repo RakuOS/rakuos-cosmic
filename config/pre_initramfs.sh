e36877e85d5b0ae7633c873d5dbdac234f58ed89
tohurtv <joshwebb84@outlook.com>
2026-04-10 21:19:31 -0600

    clean up

--- config/pre_initramfs.sh
+++ config/pre_initramfs.sh
@@ -2,6 +2,4 @@
 set -euo pipefail
 FEDORA_VERSION=$(rpm -E %fedora)
 
-echo "Swapping Fedora kernel for CachyOS kernel..."
-dnf5.real -y copr enable bieszczaders/kernel-cachyos fedora-${FEDORA_VERSION}-x86_64
-dnf5.real swap -y kernel kernel-cachyos
+echo "Preparing initramfs for RakuOS Linux Live Isos"
