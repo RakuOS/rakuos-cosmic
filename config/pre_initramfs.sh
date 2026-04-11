9587931af41e36efe22cbdb22545e7c0d3a7c80d
tohurtv <joshwebb84@outlook.com>
2026-04-10 20:54:09 -0600

    updates

--- config/pre_initramfs.sh
+++ config/pre_initramfs.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 echo "Swapping Fedora kernel for CachyOS kernel..."
-dnf5 -y copr enable bieszczaders/kernel-cachyos
-dnf5 swap -y kernel kernel-cachyos
+dnf5.real -y copr enable bieszczaders/kernel-cachyos
+dnf5.real swap -y kernel kernel-cachyos
