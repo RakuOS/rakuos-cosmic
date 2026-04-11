c0a98122b8505b41997ccdeea42e2360734d5eae
tohurtv <joshwebb84@outlook.com>
2026-04-10 21:05:23 -0600

    copr fix

--- config/pre_initramfs.sh
+++ config/pre_initramfs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
+FEDORA_VERSION=$(rpm -E %fedora)
 
 echo "Swapping Fedora kernel for CachyOS kernel..."
-dnf5.real -y copr enable bieszczaders/kernel-cachyos
+dnf5.real -y copr enable bieszczaders/kernel-cachyos fedora-${FEDORA_VERSION}-x86_64
 dnf5.real swap -y kernel kernel-cachyos
